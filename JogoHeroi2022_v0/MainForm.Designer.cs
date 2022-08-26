1dfc14fe5e4c3637576b32afc41be99f032e31e6
luisfelipe2104 <gomesluisfelipe09@gmail.com>
2022-08-26 14:26:48 -0300

    beta0.1

--- JogoHeroi2022_v0/MainForm.Designer.cs
+++ JogoHeroi2022_v0/MainForm.Designer.cs
@@ -15,8 +15,6 @@ namespace JogoHeroi2022_v0
 		/// </summary>
 		private System.ComponentModel.IContainer components = null;
 		private System.Windows.Forms.PictureBox pictureBox1;
-		private System.Windows.Forms.Button button2;
-		private System.Windows.Forms.PictureBox pictureBox2;
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -41,61 +39,30 @@ namespace JogoHeroi2022_v0
 		{
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
 			this.pictureBox1 = new System.Windows.Forms.PictureBox();
-			this.button2 = new System.Windows.Forms.Button();
-			this.pictureBox2 = new System.Windows.Forms.PictureBox();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// pictureBox1
 			// 
 			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-			this.pictureBox1.Location = new System.Drawing.Point(1, 0);
+			this.pictureBox1.Location = new System.Drawing.Point(225, 123);
 			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(860, 538);
+			this.pictureBox1.Size = new System.Drawing.Size(384, 203);
 			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabIndex = 0;
 			this.pictureBox1.TabStop = false;
-			// 
-			// button2
-			// 
-			this.button2.BackColor = System.Drawing.Color.OrangeRed;
-			this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
-			this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-			this.button2.FlatAppearance.BorderSize = 0;
-			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.button2.Font = new System.Drawing.Font("Segoe Print", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button2.Location = new System.Drawing.Point(341, 204);
-			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(193, 76);
-			this.button2.TabIndex = 2;
-			this.button2.Text = "Play";
-			this.button2.UseVisualStyleBackColor = false;
-			this.button2.Click += new System.EventHandler(this.Button2Click);
-			// 
-			// pictureBox2
-			// 
-			this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
-			this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-			this.pictureBox2.Location = new System.Drawing.Point(12, 288);
-			this.pictureBox2.Name = "pictureBox2";
-			this.pictureBox2.Size = new System.Drawing.Size(103, 164);
-			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-			this.pictureBox2.TabIndex = 3;
-			this.pictureBox2.TabStop = false;
+			this.pictureBox1.Click += new System.EventHandler(this.PictureBox1Click);
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(862, 474);
-			this.Controls.Add(this.pictureBox2);
-			this.Controls.Add(this.button2);
+			this.ClientSize = new System.Drawing.Size(834, 454);
 			this.Controls.Add(this.pictureBox1);
 			this.Name = "MainForm";
 			this.Text = "JogoHeroi2022_v0";
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainFormKeyDown);
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
 			this.ResumeLayout(false);
 
 		}
--- JogoHeroi2022_v0/MainForm.cs
+++ JogoHeroi2022_v0/MainForm.cs
@@ -22,15 +22,60 @@ namespace JogoHeroi2022_v0
 		{
 			InitializeComponent();
 			
-			pictureBox1.Visible = false;
-			pictureBox2.Visible = false;
+			this.Height = Screen.PrimaryScreen.Bounds.Height / 2 + Screen.PrimaryScreen.Bounds.Height / 4 + 150;
+			this.Width = Screen.PrimaryScreen.Bounds.Width / 2 + Screen.PrimaryScreen.Bounds.Width / 4;
+			
+			pictureBox1.Left = this.Width/4;
+			pictureBox1.Top = this.Height/4;
+			
 		}
-		void Button2Click(object sender, EventArgs e)
+	
+		PictureBox heroi = new PictureBox();
+		PictureBox fundo = new PictureBox();
+		
+		void PictureBox1Click(object sender, EventArgs e)
 		{
-			button2.Visible = false;
-			pictureBox1.Visible = true;
-			pictureBox2.Visible = true;
-			pictureBox2.Parent = pictureBox1;
+			this.KeyPreview = true;
+			
+			pictureBox1.Visible = false;
+			
+			// Tela de fundo (cenário)
+			
+			
+			fundo.Parent = this;
+			fundo.Width = this.Width;
+			fundo.Height = this.Height - 150;
+			fundo.Load("cenario2.jpg");
+			fundo.SizeMode = PictureBoxSizeMode.StretchImage;
+			
+			// Heroi 
+			
+			
+			heroi.Parent = fundo;
+			heroi.Left = 50;
+			heroi.Top = fundo.Height/2 + fundo.Height/14;
+			heroi.SizeMode = PictureBoxSizeMode.StretchImage;
+			heroi.BackColor = Color.Transparent;
+			
+			heroi.Load("heroi.gif");
+			heroi.Width = 80;
+			heroi.Height = 170;
+			
+			
+			
+		}
+		void MainFormKeyDown(object sender, KeyEventArgs e)
+		{
+			if(e.KeyCode == Keys.W || e.KeyCode == Keys.Up)
+                heroi.Top -= 15;
+            if(e.KeyCode == Keys.A || e.KeyCode == Keys.Left)
+                heroi.Left -= 15;
+            if(e.KeyCode == Keys.S || e.KeyCode == Keys.Down)
+            	if(heroi.Top + heroi.Height < fundo.Height)
+                	heroi.Top += 15;
+            if(e.KeyCode == Keys.D || e.KeyCode == Keys.Right)
+                heroi.Left += 15;
+            
 		}
 		
 	}
